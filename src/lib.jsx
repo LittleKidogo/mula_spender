e1beb93e78d1293c9b47f1f70f5b0ae9cdc10170
zacck <coderv63@gmail.com>
2017-07-05 13:00:49 +0200

    set up react and webpack to start working on atoms in the lib

---
+++ src/lib.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <h1>This is my component lib</h1>,
+  document.getElementById('lib')
+);
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+
+var DEVELOPMENT = process.env.NODE_ENV === 'development';
+var PRODUCTION = process.env.NODE_ENV === 'production';
+var TEST = process.env.NODE_ENV === 'test'
+
+module.exports = {
+    entry: "./src/lib.jsx",
+    output:  {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].bundle.js",
+      publicPath: "/"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: [{
+            loader: "babel-loader",
+            options: {presets:["react","es2015","stage-0"]}
+          }],
+          exclude: /(node_modules)/
+        }
+      ]
+    },
+    resolve:{
+      modules:[
+        "node_modules",
+        path.resolve(__dirname, "src")
+      ],
+      extensions: [
+        ".js", ".jsx"
+      ],
+    },
+    performance: {
+      hints: "warning",
+      maxAssetSize: 200000,
+      maxEntrypointSize: 400000
+    },
+    devtool: "source-map",
+    context: __dirname,
+    target: "web",
+    stats: {
+      assets: true,
+      colors: true,
+      errors: true,
+      errorDetails: true,
+      hash: true
+    },
+    devServer:{
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      historyApiFallback: true,
+      hot: true,
+      noInfo: true
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        }
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+         compress: {
+           warnings: false,
+           drop_console: false,
+         }
+       }),
+       new HtmlWebpackPlugin({
+        title: 'Component Library',
+        template: 'index-template.html'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+       name: 'common' // Specify the common bundle's name.
+     })
+    ]
+}
