fcb13869396f7b26daf872fa5122a3b845da88a6
zacck <coderv63@gmail.com>
2017-07-06 09:50:17 +0200

    work on adding paragraph example

--- src/Paragraph.jsx
+++ src/Paragraph.jsx
@@ -1,13 +1,24 @@
 import React, {Component} from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
 export class Paragraph extends Component {
   constructor(props) {
     super(props)
   }
   render() {
-    const Par = styled.p``
-    const Wrapper = styled.section``
+    const Par = styled.p`
+      ${props => props.title && css `
+          font-weight: bold;
+          text-align: center;
+        `}
+    `
+    const Wrapper = styled.section`
+      display: flex;
+      flex-direction: column;
+      ${props => props.horizontal && css `
+          flex-direction: row;
+        `}
+    `
     return(
       <Wrapper>
         <Par>
@@ -16,6 +27,11 @@ export class Paragraph extends Component {
         <Par>
           More Examples
         </Par>
+        <Wrapper horizontal>
+          <Par>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </Par>
+        </Wrapper>
       </Wrapper>
     );
   };
