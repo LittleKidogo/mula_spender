affcdd5894bf0ce54be3def51e6d45721d597ce1
Zacck Osiemo <coderv63@gmail.com>
2018-02-18 06:59:40 +0200

    enable the Api scope

--- lib/spender_web/router.ex
+++ lib/spender_web/router.ex
@@ -20,10 +20,14 @@ defmodule SpenderWeb.Router do
     pipe_through :browser # Use the default browser stack
     get "/", WelcomeController, :index
     get "/hello/:messenger", WelcomeController, :show
+
+    resources "/users", UserController, only: [:index, :show, :edit, :update]
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", SpenderWeb do
-  #   pipe_through :api
-  # end
+   scope "/api", SpenderWeb do
+     pipe_through :api
+
+     resources "/reviews", ReviewController
+   end
 end
