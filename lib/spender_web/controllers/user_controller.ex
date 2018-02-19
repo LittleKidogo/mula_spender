dc9ea3bfd750795b494633fd39f61dccc7d84ea8
Zacck Osiemo <coderv63@gmail.com>
2018-02-19 20:43:30 +0200

    add function to show users

--- lib/spender_web/controllers/user_controller.ex
+++ lib/spender_web/controllers/user_controller.ex
@@ -4,4 +4,8 @@ defmodule SpenderWeb.UserController do
   alias Spender.User
   alias Spender.Repo
 
+  def index(conn, _params) do
+    users = Repo.all(User)
+    render conn, "index.json", users: users
+  end
 end
