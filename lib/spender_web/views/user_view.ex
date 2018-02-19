3cccf558bd0e1fff5c86b533a1294d294fb21c38
Zacck Osiemo <coderv63@gmail.com>
2018-02-19 20:44:41 +0200

    Add functions to render json

--- lib/spender_web/views/user_view.ex
+++ lib/spender_web/views/user_view.ex
@@ -1,6 +1,14 @@
 defmodule SpenderWeb.UserView do
   use SpenderWeb, :view
 
+  def render("index.json", %{users: users}) do
+    %{users: Enum.map(users, &user_json/1)}
+  end
+
+  def user_json(user) do
+    %{name: user.name}
+  end 
+
   def render("show.json", %{user: user}) do
     %{data: render_one(user, SpenderWeb, "user.json")}
   end
