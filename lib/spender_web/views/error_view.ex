bb91670ed9b1bfe7f1b5fbcfa1207a250b70c2c7
Zacck Osiemo <coderv63@gmail.com>
2018-02-19 07:43:35 +0200

    wow!

--- lib/spender_web/views/error_view.ex
+++ lib/spender_web/views/error_view.ex
@@ -1,9 +1,7 @@
 defmodule SpenderWeb.ErrorView do
   use SpenderWeb, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
+
 
   def render("500.html", _assigns) do
     "Internal server error"
--- lib/spender_web/views/welcome_view.ex
+++ lib/spender_web/views/welcome_view.ex
@@ -1,3 +1,8 @@
 defmodule SpenderWeb.WelcomeView do
   use SpenderWeb, :view
+
+
+  def message do
+    "message"
+  end
 end
