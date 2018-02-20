2957cda02eed712c85bbdca6cbeba86bd2d81e9e
Zacck Osiemo <coderv63@gmail.com>
2018-02-20 10:13:50 +0200

    Add authentication pipeline, Add error handler for auth, Add authentication services, handle auth routes in the app

--- lib/spender_web/router.ex
+++ lib/spender_web/router.ex
@@ -4,6 +4,15 @@ defmodule SpenderWeb.Router do
   """
   use SpenderWeb, :router
 
+  pipeline :auth do
+    plug Spender.Auth.Pipeline
+  end
+
+  pipeline :ensure_auth do
+    plug Guardian.Plug.EnsureAuthenticated
+  end
+
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -14,6 +23,22 @@ defmodule SpenderWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  # maybe logged in
+  scope "/", SpenderWeb do
+    pipe_through [:browser, :auth]
+
+    get "/", UserController, :index
+    post "/", UserController, :login
+    post "/logout", UserController, :logout
+  end
+
+  # Definitely logged in
+  scope "/", SpenderWeb do
+    pipe_through [:browser, :auth, :ensure_auth]
+
+    get "/secret", UserController, :secret
   end
 
   scope "/api", SpenderWeb do
