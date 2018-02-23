827ffcb278acdac4e1bef7052bdc945192dd07f8
Zacck Osiemo <coderv63@gmail.com>
2018-02-23 10:37:10 +0200

    add authentication redirect test

---
+++ test/lib/spender_web/controllers/auth_controller_test.exs
@@ -0,0 +1,11 @@
+defmodule SpenderWeb.AuthControllerTest do
+  use SpenderWeb.ConnCase
+
+  describe "AuthController" do
+    test "redirects user to Google for Authentication", %{conn: conn} do
+      conn = get conn, "/auth/google?scope=email?profile"
+      assert redirected_to(conn, 302)
+    end
+  end
+
+end
