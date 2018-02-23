4f368bc21d6a5917a3be5881f1a55942eae933c9
Zacck Osiemo <coderv63@gmail.com>
2018-02-23 12:11:10 +0200

    move view tests

---
+++ test/lib/spender_web/views/error_view_test.exs
@@ -0,0 +1,17 @@
+defmodule SpenderWeb.ErrorViewTest do
+  use SpenderWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+
+  test "render 500.html" do
+    assert render_to_string(SpenderWeb.ErrorView, "500.html", []) ==
+           "Internal server error"
+  end
+
+  test "render any other" do
+    assert render_to_string(SpenderWeb.ErrorView, "505.html", []) ==
+           "Internal server error"
+  end
+end
---
+++ test/lib/spender_web/views/page_view_test.exs
@@ -0,0 +1,3 @@
+defmodule SpenderWeb.PageViewTest do
+  use SpenderWeb.ConnCase, async: true
+end
