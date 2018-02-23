dcc4d3d4ce418fc1c92a6291bf15be5fe601e927
Zacck Osiemo <coderv63@gmail.com>
2018-02-23 12:05:01 +0200

    add initial route

--- lib/spender_web/controllers/auth_controller.ex
+++ lib/spender_web/controllers/auth_controller.ex
@@ -4,12 +4,11 @@ defmodule SpenderWeb.AuthController do
 
   alias Spender.{Accounts, Accounts.User}
 
-  def index(conn, _) do
-    text conn, "welcome are you logged in?"
+  def welcome(conn, _params) do
+    text conn, "Welcome to Our Api"
   end
 
-
-
+  # handle callback payload
   def new(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     user_params = %{token: auth.credentials.token, first_name: auth.info.first_name, last_name: auth.info.last_name, email: auth.info.email, provider: "google"}
     _changeset = User.changeset(%User{}, user_params)
