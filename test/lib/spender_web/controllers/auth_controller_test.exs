222504fffca67d609638856345c3c1f9bfcdf488
Zacck Osiemo <coderv63@gmail.com>
2018-02-28 10:41:48 +0200

    test that user is created when valid attributes are passed in from google

--- test/lib/spender_web/controllers/auth_controller_test.exs
+++ test/lib/spender_web/controllers/auth_controller_test.exs
@@ -1,10 +1,27 @@
 defmodule SpenderWeb.AuthControllerTest do
   use SpenderWeb.ConnCase
 
+  alias Spender.{Accounts.User, Repo}
+
+  @ueberauth_auth %{credentials: %{token: "kbikn86917njbn"}, info: %{first_name: "Zacck", last_name: "Osiemo", email: "zacck@moneylog.com"}, provider: "google",}
+
   describe "AuthController" do
     test "redirects user to Google for Authentication", %{conn: conn} do
       conn = get conn, "/auth/google"
       assert redirected_to(conn, 302)
+    end
+
+    test "creates user from google information",%{conn: conn} do
+      #mock a response from google
+      _conn = conn
+      |> assign(:ueberauth_auth, @ueberauth_auth)
+      |> get("/auth/google/callback") #use the response
+
+      user = Repo.get_by(User, email: @ueberauth_auth.info.email)
+
+      assert Repo.aggregate(User, :count, :id) == 1
+
+      assert user.email == @ueberauth_auth.info.email
     end
   end
 
