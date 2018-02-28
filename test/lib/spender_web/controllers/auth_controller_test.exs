81cd67f54c2936f7202fe25d2d574aa399ea7312
Zacck Osiemo <coderv63@gmail.com>
2018-02-28 11:02:01 +0200

    add test for auth controller to return user

--- test/lib/spender_web/controllers/auth_controller_test.exs
+++ test/lib/spender_web/controllers/auth_controller_test.exs
@@ -2,6 +2,7 @@ defmodule SpenderWeb.AuthControllerTest do
   use SpenderWeb.ConnCase
 
   alias Spender.{Accounts.User, Repo}
+  alias SpenderWeb.AuthController
 
   @ueberauth_auth %{credentials: %{token: "kbikn86917njbn"}, info: %{first_name: "Zacck", last_name: "Osiemo", email: "zacck@moneylog.com"}, provider: "google",}
 
@@ -11,16 +12,22 @@ defmodule SpenderWeb.AuthControllerTest do
       assert redirected_to(conn, 302)
     end
 
-    test "creates user from google information",%{conn: conn} do
+    test "creates and returns user from google information",%{conn: conn} do
       #mock a response from google
-      _conn = conn
+      conn = conn
       |> assign(:ueberauth_auth, @ueberauth_auth)
-      |> get("/auth/google/callback") #use the response
+      #|> get("/auth/google/callback") #use the response
+      processed_conn = AuthController.new(conn, [])
 
+      assert json_response(processed_conn, 302) == %{"users" => [%{
+        "token" => @ueberauth_auth.credentials.token,
+        "first_name" => @ueberauth_auth.info.first_name,
+        "last_name" =>  @ueberauth_auth.info.last_name,
+        "email" => @ueberauth_auth.info.email
+        }]
+      }
       user = Repo.get_by(User, email: @ueberauth_auth.info.email)
-
       assert Repo.aggregate(User, :count, :id) == 1
-
       assert user.email == @ueberauth_auth.info.email
     end
   end
