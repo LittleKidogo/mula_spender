8cb3da1340bdc6be674102cd1d3c5bea109dc59c
Zacck Osiemo <coderv63@gmail.com>
2018-03-01 10:38:01 +0200

    Update tests to work with JaSerialiser

--- test/lib/spender_web/controllers/auth_controller_test.exs
+++ test/lib/spender_web/controllers/auth_controller_test.exs
@@ -1,8 +1,7 @@
 defmodule SpenderWeb.AuthControllerTest do
-  use SpenderWeb.ConnCase
+  use SpenderWeb.ApiCase
 
   alias Spender.{Accounts.User, Repo}
-  alias SpenderWeb.AuthController
 
   @ueberauth_auth %{credentials: %{token: "kbikn86917njbn"}, info: %{first_name: "Zacck", last_name: "Osiemo", email: "zacck@moneylog.com"}, provider: "google",}
 
@@ -16,16 +15,9 @@ defmodule SpenderWeb.AuthControllerTest do
       #mock a response from google
       conn = conn
       |> assign(:ueberauth_auth, @ueberauth_auth)
-      #|> get("/auth/google/callback") #use the response
-      processed_conn = AuthController.new(conn, [])
+      |> get("/auth/google/callback") #use the response
 
-      assert json_response(processed_conn, 302) == %{"users" => [%{
-        "token" => @ueberauth_auth.credentials.token,
-        "first_name" => @ueberauth_auth.info.first_name,
-        "last_name" =>  @ueberauth_auth.info.last_name,
-        "email" => @ueberauth_auth.info.email
-        }]
-      }
+      assert json_response(conn, 200)
       user = Repo.get_by(User, email: @ueberauth_auth.info.email)
       assert Repo.aggregate(User, :count, :id) == 1
       assert user.email == @ueberauth_auth.info.email
