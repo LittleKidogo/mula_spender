d097c8c7c4c4a4e0b18e60499e334e411c27952c
Zacck Osiemo <coderv63@gmail.com>
2018-03-05 08:58:53 +0200

    Use module tag to track application version

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Spender.Mixfile do
   use Mix.Project
+  
+  @version "0.0.1-rcauth"
 
   def project do
     [
       app: :spender,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
