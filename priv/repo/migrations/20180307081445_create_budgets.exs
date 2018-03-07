e6d4be5eea0a01bf01a8968234505f8d3266c2fe
Zacck Osiemo <coderv63@gmail.com>
2018-03-07 10:31:43 +0200

    add migration to create budgets

---
+++ priv/repo/migrations/20180307081445_create_budgets.exs
@@ -0,0 +1,19 @@
+defmodule Spender.Repo.Migrations.CreateBudgets do
+  use Ecto.Migration
+
+  def change do
+    create table(:budgets) do
+      add :name, :string
+      add :refined, :boolean, default: false, null: false
+      add :amnt_in, :float
+      add :amnt_out, :float
+      add :is_active, :boolean, default: false, null: false
+      add :start_date, :date
+      add :end_date, :date
+      add :status, :string
+
+      timestamps()
+    end
+
+  end
+end
