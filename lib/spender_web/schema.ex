2666ff0a99cf9b248c5f489694fb5d645fe13751
Zacck Osiemo <coderv63@gmail.com>
2018-03-08 18:25:46 +0200

    add a graphql schema for the application

---
+++ lib/spender_web/schema.ex
@@ -0,0 +1,22 @@
+defmodule SpenderWeb.Schema do
+  use Absinthe.Schema
+
+  alias Spender.{Accounts}
+  alias SpenderWeb.UserResolver
+
+  object :user do
+    field :avatar, non_null(:string)
+    field :first_name, non_null(:string)
+    field :last_name, non_null(:string)
+    field :email, non_null(:string)
+    field :provider, non_null(:string)
+    field :token, non_null(:string)
+  end
+
+  query do
+    field :all_users, non_null(list_of(non_null(:user))) do
+      resolve &UserResolver.all_users/3
+    end
+  end
+
+end
