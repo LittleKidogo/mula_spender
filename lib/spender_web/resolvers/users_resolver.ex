2880b82d0fbe1d03e85842586e7c433a9c554795
Zacck Osiemo <coderv63@gmail.com>
2018-03-08 19:27:36 +0200

    use accounts context to create user

--- lib/spender_web/resolvers/users_resolver.ex
+++ lib/spender_web/resolvers/users_resolver.ex
@@ -1,8 +1,14 @@
 defmodule SpenderWeb.UserResolver do
-  alias Spender.Accounts
+  alias Spender.{Accounts, Accounts.User}
 
   def all_users(_root, _args, _info) do
     users = Accounts.list_users
     {:ok, users}
+  end
+
+  def create_user(_root, args, _info) do
+    with {:ok, %User{} = user} <- Accounts.create_user(args) do
+      {:ok, user}
+    end
   end
 end
