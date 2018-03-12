a6f1e0bb214b3b9ae94ab8036c96712b9fe05631
Zacck Osiemo <coderv63@gmail.com>
2018-03-12 08:15:14 +0200

    refactor accounts test to use ex_machina

--- test/lib/spender/accounts/accounts_test.exs
+++ test/lib/spender/accounts/accounts_test.exs
@@ -10,27 +10,19 @@ defmodule Spender.AccountsTest do
     @update_attrs %{email: "someupdatedemail", provider: "someupdatedprovider", token: "someupdatedtoken"}
     @invalid_attrs %{email: "", provider: "", token: ""}
 
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_user()
-
-      user
-    end
 
     test "list_users/0 returns all users" do
-      user = user_fixture()
+      user = insert(:user)
       assert Accounts.list_users() == [user]
     end
 
     test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
+      user = insert(:user)
       assert Accounts.get_user!(user.id) == user
     end
 
     test "get_by_email/1 returns the user with given email" do
-      user = user_fixture()
+      user = insert(:user)
       assert Accounts.get_by_email(user.email) == user
     end
 
@@ -46,7 +38,7 @@ defmodule Spender.AccountsTest do
     end
 
     test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
+      user = insert(:user)
       assert {:ok, user} = Accounts.update_user(user, @update_attrs)
       assert %User{} = user
       assert user.provider == "someupdatedprovider"
@@ -55,19 +47,19 @@ defmodule Spender.AccountsTest do
     end
 
     test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
+      user = insert(:user)
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
       assert user == Accounts.get_user!(user.id)
     end
 
     test "delete_user/1 deletes the user" do
-      user = user_fixture()
+      user = insert(:user)
       assert {:ok, %User{}} = Accounts.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
     end
 
     test "change_user/1 returns a user changeset" do
-      user = user_fixture()
+      user = insert(:user)
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
