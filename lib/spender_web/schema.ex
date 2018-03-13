8d62d8f89f65597cff20348de537448996e3d49b
Zacck Osiemo <coderv63@gmail.com>
2018-03-13 08:58:17 +0200

    remove extra validation for all_users query

--- lib/spender_web/schema.ex
+++ lib/spender_web/schema.ex
@@ -14,7 +14,7 @@ defmodule SpenderWeb.Schema do
   end
 
   query do
-    field :all_users, non_null(list_of(non_null(:user))) do
+    field :all_users, list_of(:user) do
       resolve &UserResolver.all_users/3
     end
   end
