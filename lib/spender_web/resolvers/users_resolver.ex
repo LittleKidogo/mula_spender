ca55ed7238238045d1219af639435878e444a88e
Zacck Osiemo <coderv63@gmail.com>
2018-03-13 09:44:22 +0200

    add resolver to find user by email

--- lib/spender_web/resolvers/users_resolver.ex
+++ lib/spender_web/resolvers/users_resolver.ex
@@ -1,9 +1,18 @@
 defmodule SpenderWeb.UserResolver do
+  @moduledoc """
+  This resolver returns the data for the requested user fields
+  """
   alias Spender.{Accounts, Accounts.User}
 
   def all_users(_root, _args, _info) do
     users = Accounts.list_users
     {:ok, users}
+  end
+
+  def find_user(_parent, %{email: email}, _resolution) do
+    with user <- Accounts.get_by_email(email) do
+      {:ok, user}
+    end
   end
 
   def create_user(_root, args, _info) do
