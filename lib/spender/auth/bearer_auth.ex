438ec31de72b6fd2889e66e777c42d6875389ae2
Zacck Osiemo <coderv63@gmail.com>
2018-03-16 09:43:25 +0200

    look for token in header and load the associated user

--- lib/spender/auth/bearer_auth.ex
+++ lib/spender/auth/bearer_auth.ex
@@ -7,7 +7,7 @@ defmodule Spender.Auth.BearerAuth do
   # If there is a session token, validate it
   plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
   # If there is an authorization header, validate it
-  plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}
+  plug Guardian.Plug.VerifyHeader, realm: "Bearer"
   # Load the user if either of the verifications worked allow_blank so that Guardian doesnt throw an error when no user
   plug Guardian.Plug.LoadResource, allow_blank: true
 end
