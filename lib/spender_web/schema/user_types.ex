bc49acc8b69c25073f3aad81ba786f68f2115ed3
Zacck Osiemo <coderv63@gmail.com>
2018-03-26 19:50:05 +0200

    make user input non-null

--- lib/spender_web/schema/user_types.ex
+++ lib/spender_web/schema/user_types.ex
@@ -13,10 +13,10 @@ defmodule SpenderWeb.Schema.UserTypes do
 
     @desc "input for a user object"
     input_object :user_input do
-      field :email, non_null(:string)
-      field :provider, non_null(:string)
-      field :token, non_null(:string)
-      field :id, non_null(:integer)
+      field :email, :string
+      field :provider, :string
+      field :token, :string
+      field :id, :integer
       field :avatar, :string
       field :first_name, :string
       field :last_name, :string
