c9aa885018cd308aa795a591cf6f3d9dfa61be01
Zacck Osiemo <coderv63@gmail.com>
2018-03-26 16:52:22 +0200

    add input type for user

--- lib/spender_web/schema/user_types.ex
+++ lib/spender_web/schema/user_types.ex
@@ -1,7 +1,5 @@
 defmodule SpenderWeb.Schema.UserTypes do
   use Absinthe.Schema.Notation
-
-
     @desc "A system user"
     object :user do
       field :avatar, :string
@@ -10,5 +8,17 @@ defmodule SpenderWeb.Schema.UserTypes do
       field :email, :string
       field :provider, :string
       field :token, :string
+      field :id, :integer
+    end
+
+    @desc "input for a user object"
+    input_object :user_input do
+      field :email, non_null(:string)
+      field :provider, non_null(:string)
+      field :token, non_null(:string)
+      field :id, non_null(:integer)
+      field :avatar, :string
+      field :first_name, :string
+      field :last_name, :string
     end
 end
