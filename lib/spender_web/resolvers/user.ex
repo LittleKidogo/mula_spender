b38092145fcf3d818dfb2b44891f9a2a88d46511
Zacck Osiemo <coderv63@gmail.com>
2018-03-26 16:51:27 +0200

    add function to update a user

--- lib/spender_web/resolvers/user.ex
+++ lib/spender_web/resolvers/user.ex
@@ -1,11 +1,17 @@
 defmodule SpenderWeb.Resolvers.User do
-  alias Spender.Accounts
+  alias Spender.{Accounts, Accounts.User}
 
   def users(_, _, _) do
     {:ok, Accounts.list_users()}
   end
-  
+
   def user(_, %{id: id}, _) do
     {:ok, Accounts.get_user!(id)}
+  end
+
+  def update_user(_, %{input: params}, _) do
+    with {:ok, %User{} = user} <- Accounts.update_user(params) do
+      {:ok, user}
+    end
   end
 end
