eb97936e29f4142caf3161b07d3544160231ae51
Zacck Osiemo <coderv63@gmail.com>
2018-03-26 10:48:13 +0200

    add test for wishlist item creation

--- test/lib/spender/wish_list/wish_list_test.exs
+++ test/lib/spender/wish_list/wish_list_test.exs
@@ -10,6 +10,21 @@ defmodule Spender.WishListTest do
       assert Enum.count(fetched_items) == 25
     end
 
+    test "add item should add an item and update a budgets status" do
+      budget = insert(:budget)
+      attrs = %{name: "Oats", price: 25.34}
+      assert Repo.aggregate(Budget, :count, :id) == 1
+      assert Repo.aggregate(Item, :count, :id) == 0
+      {:ok, item} = WishList.add_item(budget, attrs)
+      assert Repo.aggregate(Budget, :count, :id) == 1
+      assert Repo.aggregate(Item, :count, :id) == 1
+      saved_item = Repo.one(Item)
+      saved_budget = Repo.one(Budget)
+      assert saved_item.budget_id == budget.id
+      assert saved_item.name == attrs["name"]
+      assert saved_budget.id == budget.id
+      assert saved_budget.status == "planning"
+     end
   end
 
 end
