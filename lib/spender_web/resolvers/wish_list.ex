d8f1f7ef5f38f8e9c7e333cbe212c706d1214983
Zacck Osiemo <coderv63@gmail.com>
2018-03-28 13:00:14 +0200

    Add resolver functions for items and their tests

--- lib/spender_web/resolvers/wish_list.ex
+++ lib/spender_web/resolvers/wish_list.ex
@@ -7,4 +7,18 @@ defmodule SpenderWeb.Resolvers.WishList do
         {:ok, items}
       end
   end
+
+  def create_item(_,%{input: params}, _) do
+    with {:ok, budget} <- MoneyLogs.get_budget(params.budget_id),
+      {:ok, item} <- WishList.add_item(budget, params) do
+      {:ok, item}
+    end
+  end
+
+  def update_item(_,%{input: params}, _) do
+    with {:ok, item} <- WishList.get_item(params.id),
+     {:ok, updated_item} <- WishList.update_item(item, params) do
+       {:ok, updated_item}
+     end
+  end
 end
--- test/lib/spender_web/resolvers/wish_list_test.exs
+++ test/lib/spender_web/resolvers/wish_list_test.exs
@@ -69,6 +69,152 @@ defmodule SpenderWeb.Resolvers.WishListTest do
 
       assert Enum.count(items) == Enum.count(items)
     end
+
+    @tag :authenticated
+    test "update_item returns error if item doesn't exist", %{conn: conn} do
+
+      assert Repo.aggregate(Item, :count, :id) == 0
+
+      variables = %{
+        "input" => %{
+          "id" => 40,
+          "name" => "Tomatoes",
+          "price" => 24.0
+        }
+      }
+
+      query = """
+      mutation($input: WishListItemUpdateInput!) {
+        updateWishListItem(input: $input) {
+          name
+          price
+          id
+        }
+      }
+      """
+
+      res = post conn, "/graphiql", query: query, variables: variables
+      %{
+        "errors" => [error]
+      } = json_response(res, 200)
+
+      assert Repo.aggregate(Item, :count, :id) == 0
+
+      assert error["message"] == "wishlist item not found"
+    end
+
+    @tag :authenticated
+    test "update_item edits a saved item", %{conn: conn, current_user: user} do
+      owner = insert(:owner, user: user)
+      budget = insert(:budget, owner: owner)
+
+      item = insert(:wishlist_item, budget: budget)
+      assert Repo.aggregate(Item, :count, :id) == 1
+
+      variables = %{
+        "input" => %{
+          "id" => item.id,
+          "name" => "Tomatoes",
+          "price" => 24.0
+        }
+      }
+
+      query = """
+      mutation($input: WishListItemUpdateInput!) {
+        updateWishListItem(input: $input) {
+          name
+          price
+          id
+        }
+      }
+      """
+
+      res = post conn, "/graphiql", query: query, variables: variables
+      %{
+        "data" => %{
+          "updateWishListItem" => updated_item
+        }
+      } = json_response(res, 200)
+
+      assert Repo.aggregate(Item, :count, :id) == 1
+
+
+      saved_item = Repo.one(Item)
+      assert saved_item.id  == updated_item["id"]
+      assert updated_item["name"] == variables["input"]["name"]
+    end
+
+    @tag :authenticated
+    test "add_wishlist_item", %{conn: conn, current_user: user} do
+      owner = insert(:owner, user: user)
+      budget = insert(:budget, owner: owner)
+
+      assert Repo.aggregate(Item, :count, :id) == 0
+
+      variables = %{
+        "input" => %{
+          "budget_id" => budget.id,
+          "name" => "Tomatoes",
+          "price" => 24.0
+        }
+      }
+
+      query = """
+      mutation($input: WishListItemInput!) {
+        createWishListItem(input: $input) {
+          name
+          price
+          id
+        }
+      }
+      """
+
+      res = post conn, "/graphiql", query: query, variables: variables
+      %{
+        "data" => %{
+          "createWishListItem" => item
+        }
+      } = json_response(res, 200)
+
+      assert Repo.aggregate(Item, :count, :id) == 1
+
+
+      saved_item = Repo.one(Item)
+      assert saved_item.id  == item["id"]
+      assert item["name"] == variables["input"]["name"]
+    end
+
+    @tag :authenticated
+    test "add returns error if budget doesnt exist", %{conn: conn} do
+
+      variables = %{
+        "input" => %{
+          "budget_id" => 50,
+          "name" => "Tomatoes",
+          "price" => 24.0
+        }
+      }
+
+      query = """
+      mutation($input: WishListItemInput!) {
+        createWishListItem(input: $input) {
+          name
+          price
+          id
+        }
+      }
+      """
+
+      assert Repo.aggregate(Item, :count, :id) == 0
+
+      res = post conn, "/graphiql", query: query, variables: variables
+
+      %{
+        "errors" => [error]
+      } = json_response(res, 200)
+
+      assert error["message"] == "budget not found"
+    end
   end
 
 end
