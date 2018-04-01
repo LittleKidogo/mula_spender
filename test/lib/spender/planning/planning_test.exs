7f86c96945052b2af66212eb6b45d98a3dc6ec5d
Zacck Osiemo <coderv63@gmail.com>
2018-04-01 09:06:02 +0200

    Add tests to check dates in a budget and for logsections save

---
+++ test/lib/spender/planning/planning_test.exs
@@ -0,0 +1,28 @@
+defmodule Spender.PlanningTest do
+  use Spender.DataCase
+
+  alias Spender.{
+    Planning,
+    Planning.LogSection
+  }
+
+  @num_sections 5
+  describe "Planning Boundary" do
+
+    test "it should return an error if start_date and end_date are not set for a budget" do
+      no_date_attrs = %{name: "Food Lovers", start_date: nil, end_date: nil}
+      budget = insert(:budget, no_date_attrs)
+
+     {:error, message} = Planning.add_sections(budget, @num_sections)
+
+     assert message == "#{budget.name} needs a start date and an end date"
+    end
+
+    test "it should return a budget preloaded with sections" do
+      budget = insert(:budget)
+      assert Repo.aggregate(LogSection, :count, :id) == 0
+      {:ok, budget} = Planning.add_sections(budget, @num_sections)
+      assert Repo.aggregate(LogSection, :count, :id) == @num_sections
+    end
+  end
+end
