8680de02e31085c21428c736860f106fdde8fa1f
Zacck Osiemo <coderv63@gmail.com>
2018-04-16 18:39:50 +0200

    change test password to blank

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :spender, Spender.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "spender_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
