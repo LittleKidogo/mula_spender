532839e6fec3d22dc6930eafba655c5f26dab43d
Zacck Osiemo <coderv63@gmail.com>
2018-04-19 23:36:10 +0200

    Run web service in remote vcs

--- ci/staging/steps/run_staging.sh
+++ ci/staging/steps/run_staging.sh
@@ -3,4 +3,4 @@
 # lets copy over our compose incase of any changes
 scp -o "StrictHostKeyChecking no" docker-compose.yml ${1}@${2}:/${1}/spender_staging
 scp -o "StrictHostKeyChecking no" docker-compose.staging.yml ${1}@${2}:/${1}/spender_staging
-ssh -o "StrictHostKeyChecking no" ${1}@${2} "cd spender_staging; pwd; ls -l; docker-compose -f docker-compose.staging.yml up -d --build ."
+ssh -o "StrictHostKeyChecking no" ${1}@${2} "cd spender_staging; pwd; ls -l; docker-compose -f docker-compose.staging.yml up -d --build web"
