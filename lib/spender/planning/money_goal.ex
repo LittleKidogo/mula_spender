6012e611a24bd5eb15d7d7adc60608be1c4f4e28
Zacck Osiemo <coderv63@gmail.com>
2018-05-08 15:52:38 +0200

    add changeset functionality for MoneyGoals

--- lib/spender/planning/money_goal.ex
+++ lib/spender/planning/money_goal.ex
@@ -4,7 +4,8 @@ defmodule Spender.Planning.MoneyGoal do
   import Ecto.Changeset
 
   alias Spender.{
-    MoneyLogs.Budget
+    MoneyLogs.Budget,
+    Planning.MoneyGoal
   }
 
   @type t :: %__MODULE__{}
@@ -18,5 +19,18 @@ defmodule Spender.Planning.MoneyGoal do
     timestamps()
   end
 
-  
+  @spec changeset(MoneyGoal.t, map) :: Ecto.Changeset.t()
+  def changeset(%MoneyGoal{} = moneygoal, attrs) do
+    moneygoal
+    |> cast(attrs, [:name, :location, :price])
+    |> validate_required([:name, :price])
+  end
+
+  @spec create_changeset(Budget.t, map) :: Ecto.Changeset.t()
+  def create_changeset(%Budget{} = budget, attrs) do
+    %MoneyGoal{}
+    |> changeset(attrs)
+    |> put_assoc(:budget, budget)
+  end
+
 end
