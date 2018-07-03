7897027cb3f80847424b7b6510d239fdda2428e1
br4inii <moshericoe@gmail.com>
2018-07-03 19:11:04 +0300

    add test for the page controller

---
+++ test/lib/spender_web/controllers/page_controller_test.exs
@@ -0,0 +1,11 @@
+defmodule SpenderWeb.PageControllerTest do
+  use SpenderWeb.ApiCase
+  import Plug.Test
+
+  describe "Page Controller" do
+     test "renders the homepage on the root url", %{conn: conn} do
+      conn = get conn, "/"
+      assert text_response(conn, 200)
+    end
+  end
+end
