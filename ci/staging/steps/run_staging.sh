93415c86fa0eca0ecc1606e48a55de14e2b6eab2
Zacck Osiemo <coderv63@gmail.com>
2018-07-09 10:28:53 +0200

    stop copying docker-compose, add docker-compose pull before depoying

--- ci/staging/steps/run_staging.sh
+++ ci/staging/steps/run_staging.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # lets copy over our compose incase of any changes
-scp -o "StrictHostKeyChecking no" docker-compose.yml ${1}@${2}:/${1}/spender_staging
 scp -o "StrictHostKeyChecking no" docker-compose.staging.yml ${1}@${2}:/${1}/spender_staging
-ssh -o "StrictHostKeyChecking no" ${1}@${2} "cd spender_staging; pwd; ls -l; docker-compose -f docker-compose.staging.yml up -d --build web"
+ssh -o "StrictHostKeyChecking no" ${1}@${2} "cd spender_staging; docker-compose -f docker-compose.staging.yml pull; docker-compose -f docker-compose.staging.yml up -d --build web"
