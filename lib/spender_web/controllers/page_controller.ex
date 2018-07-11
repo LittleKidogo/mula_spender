dac27d03eca7b4a7e370cb234f7fb159a951082a
Zacck Osiemo <coderv63@gmail.com>
2018-07-11 17:58:56 +0200

    suggest typespecs

--- lib/spender_web/controllers/page_controller.ex
+++ lib/spender_web/controllers/page_controller.ex
@@ -4,6 +4,8 @@ defmodule SpenderWeb.PageController do
   """
   use SpenderWeb, :controller
 
+  # this function needs an @doc attribute
+  # this function needs a spec
   def index(conn, _) do
     render conn, "page.html"
   end
