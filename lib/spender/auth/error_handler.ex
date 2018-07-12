8934ecc249381628577f550ab418e5b1e82dd9bf
Zacck Osiemo <coderv63@gmail.com>
2018-07-12 09:44:48 +0200

    add doc and typespecs to error handler plug

--- lib/spender/auth/error_handler.ex
+++ lib/spender/auth/error_handler.ex
@@ -1,6 +1,16 @@
 defmodule Spender.Auth.ErrorHandler do
+  @moduledoc """
+  This module contains functions that we use to handle errors in the
+  authentication context
+  """
   use SpenderWeb, :controller
 
+  @doc """
+  This function takes a Connection Struct, an error tuple and an optional list of options
+  It then converts the error tuple as a human readable error and puts the error in the
+  Connection Struct and returns that struct
+  """
+  @spec auth_error(Plug.Conn.t(), tuple(), list()) :: Plug.Conn.t()
   def auth_error(conn, {type, _reason}, _opts) do
     conn
     |> put_status(401)
