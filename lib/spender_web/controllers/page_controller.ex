ee216757bdad8f0dfb49646b12254d7618c6230f
br4inii <moshericoe@gmail.com>
2018-07-12 16:41:06 +0300

    Added page contoller's function @doc and @spec

--- lib/spender_web/controllers/page_controller.ex
+++ lib/spender_web/controllers/page_controller.ex
@@ -3,9 +3,12 @@ defmodule SpenderWeb.PageController do
   Controller to handle display of pages when the API is accessed from the browser
   """
   use SpenderWeb, :controller
-
-  # this function needs an @doc attribute
-  # this function needs a spec
+  @doc """
+  This function takes a Connection Struct and any other parameter which may be optional
+  it then convertes this to content that can be displayed on a web page and stores it in
+  a Connection struct and returns the struct.
+  """
+  @spec index(Plug.Conn.t(), any())::Plug.Conn,t()
   def index(conn, _) do
     render conn, "page.html"
   end
