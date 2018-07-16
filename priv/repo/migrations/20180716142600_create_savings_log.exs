0cc49b7be01639ff82146b7e7743e6eced85081c
br4inii <moshericoe@gmail.com>
2018-07-16 21:38:02 +0300

    added SavingsLog schema
    added create tables function
    
    added SavingsLog schema
    
    changed migration name
    
    fixed references
    
    fixed references
    
    fixed references

--- priv/repo/migrations/20180716142600_create_savings_log.exs
+++ priv/repo/migrations/20180716142600_create_savings_log.exs
@@ -3,16 +3,17 @@ defmodule Spender.Repo.Migrations.CreateSavingsLog do
   This module holds the database schema for the SavingsLog tables,
   """
   use Ecto.Migration
-
   @doc """
-  create a table with SavingsLog details
-  """
+    create a table with SavingsLog details
+    """
   def change do
-    create table(:savings_log) do
-      add(:name, :string)
-      add(:amount, :float)
-      add(:log_category_id, references(:log_categories))
-      add(:money_log_id, references(:budgets))
+    create table(:savingslog) do
+      add :name, :string
+      add :amount, :float
+      add :log_category_id, references(:logcategories)
+      add :money_log_id, references(:budgets)
+
+      timestamp()
     end
   end
 end
