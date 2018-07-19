e2e95ad3b56a663b5ff775306b8e28b8400b154f
br4inii <moshericoe@gmail.com>
2018-07-19 21:29:36 +0300

    fixed log_category association

--- priv/repo/migrations/20180716142600_create_savings_log.exs
+++ priv/repo/migrations/20180716142600_create_savings_log.exs
@@ -1,19 +1,18 @@
-defmodule Spender.Repo.Migrations.SavingsLog do
+defmodule Spender.Repo.Migrations.CreateSavingsLog do
   @moduledoc """
   This module holds the database schema for the SavingsLog tables,
   """
   use Ecto.Migration
-  @doc """
-    create a table with SavingsLog details
-    """
-  def change do
-    create table(:savingslog) do
-      add :name, :string
-      add :amount, :float
-      add :log_category_id, references(:logcategories)
-      add :money_log_id, references(:budgets)
 
-      timestamp()
+  @doc """
+  create a table with SavingsLog details
+  """
+  def change do
+    create table(:savings_log) do
+      add(:name, :string)
+      add(:amount, :float)
+      add(:log_category_id, references(:log_categories))
+      add(:money_log_id, references(:budgets))
     end
   end
 end
