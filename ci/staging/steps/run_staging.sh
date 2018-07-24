d4afb6c85113918ad9b05c577215c4ecfa6c4ab3
Zacck Osiemo <coderv63@gmail.com>
2018-07-24 17:56:16 +0200

    debugging ssh connection

--- ci/staging/steps/run_staging.sh
+++ ci/staging/steps/run_staging.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+echo "user"
+echo ${1}
+echo "server"
+echo ${2}
 
 # lets copy over our compose incase of any changes
-scp -o "StrictHostKeyChecking no" docker-compose.staging.yml ${1}@${2}:/home/${1}/spender_staging
+scp -vvv -o "StrictHostKeyChecking no" docker-compose.staging.yml ${1}@${2}:/home/${1}/spender_staging
 ssh -o "StrictHostKeyChecking no" ${1}@${2} "cd spender_staging; docker-compose -f docker-compose.staging.yml pull; docker-compose -f docker-compose.staging.yml up -d --build web"
