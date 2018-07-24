b7497286831b6fcda7e9ca5766698bfe57b77c53
Zacck Osiemo <coderv63@gmail.com>
2018-07-24 18:36:29 +0200

    remove debugging steps

--- ci/staging/steps/run_staging.sh
+++ ci/staging/steps/run_staging.sh
@@ -2,5 +2,5 @@
 
 
 # lets copy over our compose incase of any changes
-scp -i /home/lk_admin/.ssh/id_rsa -o "StrictHostKeyChecking no" docker-compose.staging.yml ${1}@${2}:/home/${1}/spender_staging
-ssh -i /home/lk_admin/.ssh/id_rsa -o "StrictHostKeyChecking no" ${1}@${2} "cd spender_staging; docker-compose -f docker-compose.staging.yml pull; docker-compose -f docker-compose.staging.yml up -d --build web"
+scp -o "StrictHostKeyChecking no" docker-compose.staging.yml ${1}@${2}:/home/${1}/spender_staging
+ssh -o "StrictHostKeyChecking no" ${1}@${2} "cd spender_staging; docker-compose -f docker-compose.staging.yml pull; docker-compose -f docker-compose.staging.yml up -d --build web"
