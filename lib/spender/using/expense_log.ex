942b91211e9fa9c9de315cfa9af68e0cc788e26c
Wanjiku Mac <edwardscaroline32@gmail.com>
2018-08-02 11:15:14 +0300

    added association to payment method

--- lib/spender/using/expense_log.ex
+++ lib/spender/using/expense_log.ex
@@ -8,20 +8,25 @@ defmodule Spender.Using.ExpenseLog do
 
   alias Spender.{
     MoneyLogs.Budget
+    Planning.LogCategory,
+    Using.ExpenseLog,
+    Using.PaymentMethod
   }
 
   @type t :: %__MODULE__{}
 
-  #binary key setup
+  # binary key setup
   @primary_key {:id, :binary_id, autogenerate: true}
   @derive {Phoenix.Param, key: :id}
 
   schema "expenselogs" do
-    field :name, :string
-    field :desc, :string
-    field :amount, :float
-    field :expense_date, :date
-    belongs_to :budget, Budget, foreign_key: :budget_id, type: :binary_id
+    field(:name, :string)
+    field(:desc, :string)
+    field(:amount, :float)
+    field(:expense_date, :date)
+    belongs_to(:budget, Budget, foreign_key: :budget_id, type: :binary_id)
+    belongs_to(:logcategory, LogCategory, foreign_key: :logcategory_id, type: :binary_id)
+    belongs_to(:paymentmethod, PaymentMethod, foreign_key: :paymentmethod_id, type: :binary_id)
   end
 
   @doc """
@@ -30,8 +35,41 @@ defmodule Spender.Using.ExpenseLog do
   """
   @spec changeset(ExpenseLog.t(), map) :: Changeset.t()
   def changeset(expenselog, attrs \\ %{}) do
-   expenselog
-   |> cast(attrs, [:name, :desc, :amount])
-   |> validate_required([:name, :amount])
- end
+    expenselog
+    |> cast(attrs, [:name, :desc, :amount])
+    |> validate_required([:name, :amount])
+  end
+
+  @doc """
+  This changeset function takes in a budget struct and map containing parameters
+  It proceeds to match the parameters in the the map the schema above
+  """
+  @spec create_changeset(Budget.t(), map) :: Changeset.t()
+  def create_changeset(%Budget{} = budget, attrs) do
+    %ExpenseLog{}
+    |> ExpenseLog.changeset(attrs)
+    |> put_assoc(:budget, budget)
+  end
+
+  @doc """
+  This changeset function takes in a logcategory struct and map containing parameters
+  It proceeds to match the parameters in the the map the schema above
+  """
+  @spec log_changeset(LogCategory.t(), map) :: Changeset.t()
+  def log_changeset(%LogCategory{} = logcategory, attrs \\ %{}) do
+    %ExpenseLog{}
+    |> ExpenseLog.changeset(attrs)
+    |> put_assoc(:logcategory, logcategory)
+  end
+
+  @doc """
+  This changeset function takes in a paymentmethod struct and map containing parameters
+  It proceeds to match the parameters in the the map the schema above
+  """
+  @spec payment_changeset(PaymentMethod.t(), map) :: Changeset.t()
+  def payment_changeset(%PaymentMethod{} = paymentmethod, attrs \\ %{}) do
+    %ExpenseLog{}
+    |> ExpenseLog.changeset(attrs)
+    |> put_assoc(:paymentmethod, paymentmethod)
+  end
 end
