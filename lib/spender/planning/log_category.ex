5aa013ece9d6741d1ad9759b92781ff7c12bdef3
Wanjiku Mac <edwardscaroline32@gmail.com>
2018-08-02 11:08:02 +0300

    added the @doc attribute

--- lib/spender/planning/log_category.ex
+++ lib/spender/planning/log_category.ex
@@ -13,14 +13,14 @@ defmodule Spender.Planning.LogCategory do
 
   @type t :: %__MODULE__{}
 
-  #binary key setup
+  # binary key setup
   @primary_key {:id, :binary_id, autogenerate: true}
   @derive {Phoenix.Param, key: :id}
 
   schema "logcategories" do
     field(:name, :string)
     belongs_to(:budget, Budget, foreign_key: :budget_id, type: :binary_id)
-    has_many :expenselogs, ExpenseLog
+    has_many(:expenselogs, ExpenseLog)
   end
 
   @doc """
@@ -35,6 +35,10 @@ defmodule Spender.Planning.LogCategory do
     |> validate_length(:name, max: 40)
   end
 
+  @doc """
+  This changeset function takes in a budget struct and map containing parameters
+  It proceeds to match the parameters in the the map to the schema above
+  """
   @spec create_changeset(Budget.t(), map()) :: Ecto.Changeset.t()
   def create_changeset(%Budget{} = budget, attrs) do
     %LogCategory{}
